c47f391e3eb635b29597425a77d840bb3f6df524
Okram Licmeva <okramlicmeva@gmail.com>
2026-05-03 21:45:39 +0200

    if switch 4 - rad, domaci

---
+++ 4 if switch/domaci if switch/js.js
@@ -0,0 +1,32 @@
+
+let dayOfTheWeek = "utorak";
+
+switch (dayOfTheWeek) {
+
+    case "ponedeljak":
+        console.log("work work")
+        break;
+    case "utorak":
+        console.log("Work work");
+        break;
+
+    case "sreda":
+        console.log("work work");
+        break;
+
+    case "cetvrtak":
+        console.log("Work work");
+        break;
+
+    case "petak":
+        console.log("weekend is coming");
+        break;
+
+    case "subota":
+        console.log("weekend!");
+        break;
+
+    case "nedelja":
+        console.log("weekend!");
+        break;
+}
