0c508b9a0a2fbcd648608e46a7e61b94ac3f4930
Okram Licmeva <okramlicmeva@gmail.com>
2026-05-05 01:43:25 +0200

    if vezbe + loops

--- 5 loops/js.js
+++ 5 loops/js.js
@@ -2,8 +2,11 @@
 
 let name = "Admin"; // capital letter
 
+name = name.toLowerCase(); // konvertuje u malo slovo
+//name = name.toUpperCase(); // konvertuje u velika slova
+console.log(name);
 
-if(name == "admin") {
+if(name == "admin") { //targetiramo sta smo odje napisali
     console.log("Vi ste admin")
 } else {
     console.log("vi niste administrator");
